7c6afb7209fb05d1b69b73bbeaeabc400aa6da03
Iftikhar Ahmad <iftikhar.ahmad@canonical.com>
2016-11-12 18:12:31 +0500

    test branch to test travis-ci

---
+++ tests/parse-test.js
@@ -0,0 +1,8 @@
+var str='Name        Version       Rev  Developer  Notes
+core        16.04.1       448  canonical  -
+htop        2.0.2         71   maxiberta  -
+pi2         16.04-0.17    29   canonical  -
+pi2-kernel  4.4.0-1030-3  23   canonical  -
+snapweb     0.21.2        25   canonical  -';
+
+console.log(str);
