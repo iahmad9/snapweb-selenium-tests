0c09c86c822ba36b0c1acc8ec76d12e83b78ff7b
Iftikhar Ahmad <iftikhar.ahmad@canonical.com>
2016-11-14 13:08:01 +0500

    build the snap

--- build-snap.sh
+++ build-snap.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 pwd
-mkdir $HOME/tmp
-export GOPATH=$HOME/tmp
-cd tmp
+#mkdir $HOME/tmp
+#export GOPATH=$HOME/tmp
+#cd tmp
 
 #git clone -b snapweb-selenium-tests --single-branch https://github.com/snapcore/snapweb.git snapweb-selenium-tests
 
@@ -19,17 +19,17 @@ mkdir ~/node
 export PATH=$PATH:$HOME/node/bin
 export NODE_PATH=$HOME/node/lib/node_modules
 
-mkdir -p $GOPATH/src/github.com/snapcore
-cd $GOPATH/src/github.com/snapcore
-git clone -b snapweb-selenium-tests --single-branch https://github.com/snapcore/snapweb.git snapweb-selenium-tests
-cd snapweb-selenium-tests
+#mkdir -p $GOPATH/src/github.com/snapcore
+#cd $GOPATH/src/github.com/snapcore
+#git clone -b snapweb-selenium-tests --single-branch https://github.com/snapcore/snapweb.git snapweb-selenium-tests
+#cd snapweb-selenium-tests
 
 npm install -g --prefix=$(npm config get prefix) gulp
 npm install
 
 pwd
 
-cd $GOPATH/src/github.com/snapcore/snapweb
+#cd $GOPATH/src/github.com/snapcore/snapweb
 pwd
 source ./build.sh
 
