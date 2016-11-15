a092fbff32339ce1b937e03890e6dd89056ce598
Iftikhar Ahmad <iftikhar.ahmad@canonical.com>
2016-11-15 12:30:55 +0500

    build snap in docker

--- scripts/snap-in-docker.sh
+++ scripts/snap-in-docker.sh
@@ -2,6 +2,6 @@
 #
 # Build the snapweb snap using a docker container.
 
-set -ev
+set -exv
 
-docker run -v $GOPATH:/go snapcore/snapcraft sh -c 'cd /go/src/github.com/snapweb-selenium-tests && export GOPATH=/go PATH=/go/bin:$PATH && ./scripts/snap.sh'
+docker run -v $GOPATH:/go snapcore/snapcraft sh -c 'cd /go/src/github.com/snapcore/snapweb && export GOPATH=/go PATH=/go/bin:$PATH && ./scripts/snap.sh'
